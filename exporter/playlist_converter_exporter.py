ffd5b2a7f9d78f2c2e4f2ad4be4291fadab07e97
r2rstep <aart.st@gmail.com>
2018-10-28 19:18:43 +0100

    supports multiple entries

--- exporter/playlist_converter_exporter.py
+++ exporter/playlist_converter_exporter.py
@@ -7,7 +7,11 @@ import webbrowser
 class PlaylistConverterExporter(Exporter):
     def __init__(self):
         self._base_url = 'http://www.playlist-converter.net/#/freetext='
+        self._encoded_newline = '%0A'
 
     def export(self, entries: dict):
-        url = self._base_url + entries[0]['artist'] + '-' + entries[0]['title']
+        url = self._base_url
+        for entry in entries:
+            url += entry['artist'] + ' - ' + entry['title'] + self._encoded_newline
+        url = url.rstrip(self._encoded_newline)
         webbrowser.open(url)
--- exporter/test/test_playlist_converter.py
+++ exporter/test/test_playlist_converter.py
@@ -6,10 +6,23 @@ from mock import patch
 
 @patch('exporter.playlist_converter_exporter.webbrowser')
 class TestPlaylistConverter(TestCase):
-    def test_export(self, webbrowser):
+    def test_export_single_entry(self, webbrowser):
         entries = [{'artist': 'artist',
                     'title': 'title'}]
         exporter = PlaylistConverterExporter()
         exporter.export(entries)
-        webbrowser.open.assert_called_with('http://www.playlist-converter.net/#/freetext=artist-title')
+        webbrowser.open.assert_called_with('http://www.playlist-converter.net/#/freetext=artist - title')
+
+    def test_export_multiple_entries(self, webbrowser):
+        entries = [{'artist': 'Eminem',
+                    'title': 'Kim'},
+                   {'artist': 'Kanye West',
+                    'title': 'Addiction'}]
+        exporter = PlaylistConverterExporter()
+        exporter.export(entries)
+        webbrowser.open.assert_called_with('http://www.playlist-converter.net/#/freetext=Eminem - Kim'
+                                           '%0AKanye West - Addiction')
+
+    def test_export_entry_contains_http_special_character(self, webbrowser):
+        self.fail('e. g. slash')
 
