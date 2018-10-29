b75e97235147bbab84d16209407c15ba35a9593b
Artur Stepniak <artur.stepniak@nokia.com>
2018-10-29 14:21:04 +0100

    modified readme, script name and version

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # tetos
 Text to Spotify - saves time by converting text to a Spotify playlist
+
+Usage:
+tetos path_to_txt_playlist_file
--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import find_packages
 
 setup(
     name='tetos',
-    version='0.1dev',
+    version='0.2dev',
     author='Artur Stepniak',
     author_email='aart.st@gmail.com',
     packages=find_packages(),
-    scripts=['script/tetos.py'],
+    scripts=['script/tetos'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read()
 )
