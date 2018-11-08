93c831f8d15e895837595ac5b68b493e38698d18
Artur Stepniak <artur.stepniak@nokia.com>
2018-11-08 13:02:49 +0100

    removed requirement for the input path to start with file:// for local files

--- tetos/executor/args_checker.py
+++ tetos/executor/args_checker.py
@@ -1,7 +1,13 @@
-supported_protocols = ['http://', 'https://', 'ftp://', 'file://']
+import glob
+
+
+supported_protocols = ['http://', 'https://', 'ftp://']
 
 
 def check_args(args):
-    path = args.input_path.pop()
+    path = args.input_path[0]
     if not any(path.startswith(protocol) for protocol in supported_protocols):
-        raise ValueError('Path should begin with {} for a local file'.format(' or '.join(supported_protocols)))
+        if not glob.glob(path):
+            raise ValueError('Path format incorrect or local file does not exist. Path should begin with '
+                             '{supported_protocols} for remote files'
+                             .format(supported_protocols=' or '.join(supported_protocols)))
--- tetos/executor/test/test_args_checker.py
+++ tetos/executor/test/test_args_checker.py
@@ -7,6 +7,7 @@ from mock import Mock
 class TestArgsChecker(TestCase):
     def test_incorrect_input(self):
         args = Mock(input_path=['/path'])
-        with self.assertRaisesRegex(ValueError, 'Path should begin with http:// or https:// or ftp:// '
-                                                'or file:// for a local file'):
+        with self.assertRaisesRegex(ValueError, 'Path format incorrect or local file does not exist. '
+                                                'Path should begin with http:// or https:// or ftp:// '
+                                                'for remote files'):
             check_args(args)
