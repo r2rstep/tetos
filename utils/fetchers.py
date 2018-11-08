940ceb25c6d55f90849737a7012c198c08f4246b
Artur Stepniak <artur.stepniak@nokia.com>
2018-11-08 12:01:14 +0100

    using urllib handlers instead of urlopen

--- utils/fetchers.py
+++ utils/fetchers.py
@@ -11,7 +11,12 @@ def fetch_html(src: str):
         return frames
 
     try:
-        resp = url_request.urlopen(src)
+        http_handler = url_request.HTTPHandler()
+        proxy_handler = url_request.ProxyHandler()
+        opener = url_request.OpenerDirector()
+        opener.add_handler(proxy_handler)
+        opener.add_handler(http_handler)
+        resp = opener.open(src)
     except ValueError:
         raise ValueError('URL should begin with "http://" or "https://"')
 
